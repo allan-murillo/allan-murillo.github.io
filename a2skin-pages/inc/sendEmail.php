03dbac77e36040766f046da4aef28e6dce26784a
Allan Murillo <54790384+allan-murillo@users.noreply.github.com>
2023-02-04 23:00:14 +0800

    Update Main Page
    HIFU

--- a2skin-pages/inc/sendEmail.php
+++ a2skin-pages/inc/sendEmail.php
@@ -10,31 +10,11 @@ if($_POST) {
    $email = trim(stripslashes($_POST['email']));
    $subject = trim(stripslashes($_POST['phone']));
    $contact_message = trim(stripslashes($_POST['last_name']));
+   $error = "";
 
-   // Check Name
-	if (strlen($name) < 2) {
-		$message = "Please enter your name.";
-
-		echo "<script type='text/javascript'>alert('$message');</script>";
-
-		// '<script type="text/javascript">alert("Please enter your name.")</script>'; 
-		//$error['name'] = echo;
-	}
-	// Check Email
-	if (!preg_match('/^[a-z0-9&\'\.\-_\+]+@[a-z0-9\-]+\.([a-z0-9\-]+\.)*+[a-z]{2}/is', $email)) {
-
- 		echo '<script type="text/javascript">alert("Please enter a valid email address.")</script>'; 
-
-		 //$error['email'] = "Please enter a valid email address.";
-	}
-	// Check Message
-	//if (strlen($contact_message) < 15) {
-
-		 //echo '<script type="text/javascript">alert("Please enter your message. It should have at least 15 characters.")</script>';
-		// $error['message'] = "Please enter your message. It should have at least 15 characters.";
-	//}
-   // Subject
-	if ($subject == '') { $subject = "Contact Form Submission"; }
+   if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+	$error = "Invalid email address";
+  }
 
 
    // Set Message
@@ -42,7 +22,7 @@ if($_POST) {
 	$message .= "Email address: " . $email . "<br />";
    $message .= "Mobile Number: <br />";
    $message .= $subject;
-   $message .= "<br /> ----- <br /> This is from website email inquiry. <br />";
+   $message .= "<br /> ----- <br /> This is from A2Skin website email inquiry. <br />";
 
    // Set From: header
    $from =  $name . "<" . $email . ">";
@@ -54,14 +34,14 @@ if($_POST) {
 	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
 
-   if (!$error) {
+	if (empty($error)){
 
       ini_set("sendmail_from", $siteOwnersEmail); // for windows server
       $mail = mail($siteOwnersEmail, $subject, $message, $headers);
 
-			if ($mail) { header("Location: https://skinbya2.000webhostapp.com/contact.html"); /* Redirect browser */ 
+			if ($mail) { header("Location: https://a2skinmedspa.000webhostapp.com/index.html"); /* Redirect browser */ 
 exit(); }
-      else { header("Location: https://skinbya2.000webhostapp.com/contact.html"); /* Redirect browser */ 
+      else { header("Location: https://a2skinmedspa.000webhostapp.com/index.html"); /* Redirect browser */ 
        }
 
 		
